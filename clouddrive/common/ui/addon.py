18cca8cbb992e264fa7c5127975a67a3ab69af59
Laser Yuan <laser.yuan@gmail.com>
2020-05-30 18:54:01 +0800

    Show video description as plot in kodi

--- clouddrive/common/ui/addon.py
+++ clouddrive/common/ui/addon.py
@@ -418,6 +418,7 @@ class CloudDriveAddon(RemoteProcessCallable):
                     info.update(item['audio'])
                     info_type = 'music'
                 elif 'video' in item:
+                    info['plot'] = Utils.get_safe_value(item, 'description')
                     list_item.addStreamInfo('video', item['video'])
                 list_item.setInfo(info_type, info)
                 if 'thumbnail' in item:
